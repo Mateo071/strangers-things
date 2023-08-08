6bd356990b7449abb39e327766edeca1d5ec68a8
Mateo071 <floresmateo2000@gmail.com>
2023-08-08 15:37:00 -0400

    infrastructure

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from 'react';
+import Profile from './Profile.jsx'
+
 const Home = () => {
+  const [loggedIn, setLoggedIn] = useState();
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    setLoggedIn(true);
+    setUsername('Mateo')
+  }, []);
+
+  const toProfile = () => {
+    console.log('TO PROFILE')
+    return(
+      <Profile/>
+    )
+  }
+
+  const toLogIn = () => {
+    console.log('TO LOGIN')
+  }
+
+  const toSignUp = () => {
+    console.log('TO SIGN UP')
+  }
+
   return (
-    <h2>Home</h2>
+    <>
+      <h1>Welcome to Stranger's Things!</h1>
+      <h2>{loggedIn ? `Logged in as ${username}` : `Log In Here:`}</h2>
+      {
+        loggedIn ? <button onClick={() => toProfile()}>View Profile</button> : <><button onClick={() => toLogIn()}>Log In</button>
+        <button onClick={() => toSignUp()}>New User? Sign up</button></>
+      }
+    </>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,25 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const NavBar = () => {
+  const [loggedIn, setLoggedIn] = useState('');
+
+  useEffect(() => {
+    setLoggedIn('true');
+  }, []);
+
   return (
     <>
-      <Link to="/">Home</Link>
-      <Link to="/posts">Posts</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/login">Log In</Link>
+      <Link to="/">Stranger's Things</Link>
+      <Link to="/">HOME</Link>
+      <Link to="/posts">POSTS</Link>
+      {loggedIn ? 
+      <>
+        <Link to="/profile">PROFILE</Link>
+        <Link onClick={() => console.log('sign out')}>LOG OUT</Link>
+      </> : 
+      <Link to="/login">LOG IN</Link>
+      }
     </>
   )
 }
